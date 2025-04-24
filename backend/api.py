34812d2b0ccdfb7f11ef20cc2170dbf009d8b13d
IT Programmer <40290724+1IT-Programmer@users.noreply.github.com>
2025-04-24 14:35:03 +0500

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,47 +1,169 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-from typing import Optional
-from passlib.context import CryptContext
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from starlette.requests import Request
+from starlette.responses import Response
+from sqlalchemy.orm import Session
+from typing import Annotated
 from datetime import timedelta
-from jose import jwt
+from pydantic import EmailStr
+from jinja2 import TemplateNotFound
+from .database import engine, SessionLocal
+from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from .auth import authenticate_user, create_access_token, get_current_active_user
+from .models import User, Trip
+from .schemas import UserCreate, UserUpdate, TripCreate, TokenData
+from .utils import generate_unique_id
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
-templates = Jinja2Templates(directory="frontend/templates")
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.post("/login")
-async def login(email: str = Form(...), password: str = Form(...)):
-    # Реализовать проверку пароля и вход
+# Подключение сессии базы данных
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.on_event("startup")
+async def startup():
+    print("Starting server...")
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Shutting down server...")
+
+@app.post("/token", response_model=TokenData)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: Annotated[Session, Depends(get_db)]):
+    """
+    Генерирует JWT-токен после успешной авторизации.
+    """
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Неправильные учетные данные",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": email}, expires_delta=access_token_expires)
-    response = RedirectResponse(url="/dashboard")
-    response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True)
-    return response
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+@app.get("/", include_in_schema=False)
+async def read_root(request: Request):
+    """
+    Главная страница приложения (для рендеринга front-end шаблонов).
+    """
+    try:
+        template = request.app.templates.get_template("index.html")
+        return template.render()
+    except TemplateNotFound:
+        return Response(content="Template not found.", media_type="text/html", status_code=404)
 
-@app.get("/dashboard")
-async def dashboard(request: Request):
-    token = request.cookies.get("Authorization").split()[1]
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    email = payload.get("sub")
-    return f"Hello, {email}"
+@app.post("/users/", response_model=UserCreate)
+async def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    """
+    Регистрация нового пользователя.
+    """
+    db_user = User(username=user.username, hashed_password=get_password_hash(user.password), full_name=user.full_name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+@app.get("/users/{user_id}/", response_model=UserCreate)
+async def get_user(user_id: int, db: Annotated[Session, Depends(get_db)],
+                   current_user: Annotated[User, Security(get_current_active_user)]):
+    """
+    Возвращает информацию о конкретном пользователе.
+    """
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return db_user
+
+@app.put("/users/{user_id}/", response_model=UserUpdate)
+async def update_user(user_id: int, updated_user: UserUpdate, db: Annotated[Session, Depends(get_db)],
+                      current_user: Annotated[User, Security(get_current_active_user)]):
+    """
+    Редактирует профиль пользователя.
+    """
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    for key, value in updated_user.model_dump(exclude_unset=True).items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.delete("/users/{user_id}/", response_model=str)
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)],
+                      current_user: Annotated[User, Security(get_current_active_user)]):
+    """
+    Удаляет пользователя.
+    """
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    db.delete(db_user)
+    db.commit()
+    return f"Пользователь с ID {user_id} удалён."
+
+@app.post("/trips/", response_model=TripCreate)
+async def create_trip(trip: TripCreate, db: Annotated[Session, Depends(get_db)],
+                     current_user: Annotated[User, Security(get_current_active_user)]):
+    """
+    Создаёт новую поездку.
+    """
+    db_trip = Trip(departure_point=trip.departure_point, arrival_point=trip.arrival_point, distance=trip.distance, price=trip.price, user_id=current_user.id)
+    db.add(db_trip)
+    db.commit()
+    db.refresh(db_trip)
+    return db_trip
+
+@app.get("/trips/", response_model=list[TripCreate])
+async def list_trips(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+    """
+    Показывает список всех поездок.
+    """
+    return db.query(Trip).offset(skip).limit(limit).all()
+
+@app.get("/trips/{trip_id}/", response_model=TripCreate)
+async def get_trip(trip_id: int, db: Annotated[Session, Depends(get_db)]):
+    """
+    Получает детальную информацию о конкретной поездке.
+    """
+    db_trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if db_trip is None:
+        raise HTTPException(status_code=404, detail="Поездка не найдена")
+    return db_trip
+
+@app.put("/trips/{trip_id}/", response_model=TripCreate)
+async def update_trip(trip_id: int, updated_trip: TripCreate, db: Annotated[Session, Depends(get_db)]):
+    """
+    Обновляет информацию о поездке.
+    """
+    db_trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if db_trip is None:
+        raise HTTPException(status_code=404, detail="Поездка не найдена")
+    for key, value in updated_trip.model_dump(exclude_unset=True).items():
+        setattr(db_trip, key, value)
+    db.commit()
+    db.refresh(db_trip)
+    return db_trip
+
+@app.delete("/trips/{trip_id}/", response_model=str)
+async def delete_trip(trip_id: int, db: Annotated[Session, Depends(get_db)]):
+    """
+    Удаляет поездку.
+    """
+    db_trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if db_trip is None:
+        raise HTTPException(status_code=404, detail="Поездка не найдена")
+    db.delete(db_trip)
+    db.commit()
+    return f"Поездка с ID {trip_id} удалена."
